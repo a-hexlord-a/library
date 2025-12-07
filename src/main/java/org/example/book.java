addfc7c70775d026dc2557f95063469017cf78b5
Студент <student@classvivt.loc>
2025-12-07 13:50:51 +0300

    first blad

---
+++ src/main/java/org/example/book.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class book {
+    protected int id;
+    protected String title;
+    protected int avtorid;
+    protected int puplishingId;
+    protected String publishingDate;
+    protected int genreId;
+
+}
---
+++ src/main/java/org/example/user.java
@@ -0,0 +1,85 @@
+package org.example;
+
+public class user {
+    protected int id;
+    protected String firstname;
+    protected String lastname;
+    protected String midname;
+    protected String birstday;
+    protected String email;
+    protected String phon;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getMidname() {
+        return midname;
+    }
+
+    public void setMidname(String midname) {
+        this.midname = midname;
+    }
+
+    public String getBirstday() {
+        return birstday;
+    }
+
+    public void setBirstday(String birstday) {
+        this.birstday = birstday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhon() {
+        return phon;
+    }
+
+    public void setPhon(String phon) {
+        this.phon = phon;
+    }
+
+    public void User(int id){
+        this.id=id;
+        String firstname;
+         String lastname;
+         String midname;
+         String birstday;
+        String email;
+        String phon;
+    }{
+        this.birstday=birstday;
+        this.firstname=firstname;
+        this.midname=midname;
+        this.lastname=lastname;
+        this.email=email;
+        this.phon=phon;
+
+    }
+}
